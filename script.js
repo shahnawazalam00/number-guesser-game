c4730f2f1aa5bd6a9375a390061571ad24dbc1f2
Mohd Shahnawaz Alam <shahnawazalam9839@gmail.com>
2024-07-30 19:37:08 +0530

    heloo

--- script.js
+++ script.js
@@ -9,19 +9,23 @@ let x  = Math.floor(Math.random()*(max-min+1) +min );
 let attemp =0;
 button1.onclick = function(){
      attemp++;
-   if(x==mynum.value){it 
+     let y = Number(mynum.value);
+   if(x==y){
     answer.textContent=`yes it is ${x} and it took u ${attemp} attempts`;
    
    }
-   else if(x<mynum.value){
+   else if(x<y){
     answer.textContent = ` too high! try again`;
    }
    else {
     answer.textContent = `too low! try again`;
    }
+   //answer.textContent=mynum.value;
 }
 button2.onclick = function(){
     attemp=0;
     mynum.value="";
     answer.textContent="";
+    x  = Math.floor(Math.random()*(max-min+1) +min );
+
 }
